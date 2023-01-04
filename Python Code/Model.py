c7a03eb0565acac663c32d4ddc52b7a9c0a661c3
mfortuno0996 <78323502+mfortuno0996@users.noreply.github.com>
2023-01-04 18:49:13 -0500

    Code fixed
    model code fixed to have the LoadImage function operational

--- Python Code/Model.py
+++ Python Code/Model.py
@@ -2,27 +2,27 @@ import mesa
 from Agent import Exhaust
 from PIL import Image
 
-def LoadImage(filename): #loads image for model
+def LoadImage(filename):
     try:
         original_image = Image.open(filename)
     except:
         print("Unable to load image")
-    L1 = original_image.convert('RGB')
-    return L1
+    L3 = original_image.convert('RGB') #converts image to RGB
+    return L3
 
-Hellenic_Trench_Image = Image.open('Hellenic Trench Ships_Shrunk.png') #loads Hellenic Trench Image
-Hellenic_Trench_Image1 = Hellenic_Trench_Image.convert('RGB')
+
 
 class ExhaustModel(mesa.Model):
 
     def __init__(self, width=100, height=100, exhaust_prob = 0.6):
 
         self.schedule = mesa.time.RandomActivation(self)
-        self.grid = mesa.space.Grid(width, height, torus = False)
+        self.grid = mesa.space.Grid(width, height, torus=False)
 
         for (contents, x, y) in self.grid.coord_iter():
+            Hellenic_Trench_Image1 = LoadImage('Hellenic Trench Ships_Shrunk.png')
             r, g, b = Hellenic_Trench_Image1.getpixel((x,y)) #gets the pixel rgb value
-            if b > r:
+            if b > r: #navy color value
                 if self.random.random() < exhaust_prob: #checks to see if random value is less than exhaust_prob value
                     new_exhaust = Exhaust((x,y), self) #creates exhaust agent
                     self.grid.place_agent(new_exhaust, (x,y)) #places exhaust agent on grid
